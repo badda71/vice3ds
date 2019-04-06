272a532b5c920acfc3414a8ee52279acb963fbd9
badda71 <webersebastian@yahoo.de>
2019-04-06 14:07:55 +0200

    c-stick map to cursor keys, d-pad navigation in vice menu

--- src/arch/shared/archdep_atexit.c
+++ src/arch/shared/archdep_atexit.c
@@ -31,70 +31,6 @@
 
 #include "archdep_atexit.h"
 
-
-#if defined(USE_NATIVE_GTK3) && defined(WIN32_COMPILE) && !defined(__cplusplus)
-#define ATEXIT_MAX_FUNCS 64
-
-#include "debug_gtk3.h"
-
-/** \brief  List of functions registered with archdep_vice_atexit()
- *
- * The functions get called in the reverse order of which they were registered,
- * so this acts as a stack,
- */
-static void (*atexit_functions[ATEXIT_MAX_FUNCS + 1])(void);
-
-
-/** \brief  Number of register functions in #atexit_functions
- */
-static int atexit_counter = 0;
-
-
-/** \brief  Register \a function to be called on exit() or return from main
- *
- * Wrapper to work around Windows not handling the C library's atexit()
- * mechanism properly
- *
- * \param[in]   function    function to call at exit
- *
- * \return  0 on success, 1 on failure
- */
-int archdep_vice_atexit(void (*function)(void))
-{
-    debug_gtk3("registering function %p.", function);
-    if (atexit_counter == ATEXIT_MAX_FUNCS) {
-        debug_gtk3("ERROR: max atexit functions reached.");
-        return 1;
-    }
-
-    atexit_functions[atexit_counter] = function;
-    atexit_counter++;
-
-    return 0;
-}
-
-
-/** \brief  Wrapper around exit()
- *
- * \param[in]   excode  exit code
- */
-void archdep_vice_exit(int excode)
-{
-    const void (*f)(void);
-
-    debug_gtk3("unrolling atexit stack:");
-    /* don't check for NULL, segfaults allow backtraces in gdb */
-    while (atexit_counter > 0) {
-        atexit_counter--;
-        f = atexit_functions[atexit_counter];
-        debug_gtk3("running atexit %d: %p.", atexit_counter, f)
-        f();
-    }
-    exit(excode);
-}
-#else  /* ifdef WIN32_COMPILE */
-
-
 /** \brief  Register \a function to be called on exit() or return from main
  *
  * Wrapper to work around Windows not handling the C library's atexit()
@@ -118,5 +54,3 @@ void archdep_vice_exit(int excode)
 {
     exit(excode);
 }
-
-#endif
