953d9e86f6a62c914110252d282f351487d2d21b
badda71 <webersebastian@yahoo.de>
2019-05-09 13:09:51 +0200

    menu entry Misc - LOAD"*",8,1

--- source/arch/sdl/menu_misc.c
+++ source/arch/sdl/menu_misc.c
@@ -4,7 +4,7 @@
  * Written by
  *  Sebastian Weber <me@badda.de>
  *
- * This file is part of VICE, the Versatile Commodore Emulator.
+ * This file is part of VICE3DS
  * See README for copyright notice.
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -28,7 +28,8 @@
 #include "menu_common.h"
 #include "log.h"
 
-static int ui_key_press_sequence (int keyseq[][3]) {
+static int ui_key_press_sequence (void *param) {
+	int (*keyseq)[3]=param;
 	SDL_Event sdl_e;
 	for (int i=0;keyseq[i][2]!=0;++i) {
 		SDL_Delay(keyseq[i][0]);
@@ -40,11 +41,43 @@ static int ui_key_press_sequence (int keyseq[][3]) {
 }
 
 int keyseq_rsrestore[][3]={
-	{0,   SDL_KEYDOWN,  3},
-	{20,  SDL_KEYDOWN, 25},
+	{0,   SDL_KEYDOWN,  3}, //R/S
+	{20,  SDL_KEYDOWN, 25}, //Restore
 	{100, SDL_KEYUP,   25},
 	{0,   SDL_KEYUP,    3},
 	{0,   0,            0}
+};
+
+int keyseq_load[][3]={
+	{  0, SDL_KEYDOWN, 108}, //L
+	{ 60, SDL_KEYUP  , 108},
+	{ 10, SDL_KEYDOWN, 111}, //O
+	{ 60, SDL_KEYUP  , 111},
+	{ 10, SDL_KEYDOWN,  97}, //A
+	{ 60, SDL_KEYUP  ,  97},
+	{ 10, SDL_KEYDOWN, 100}, //D
+	{ 60, SDL_KEYUP  , 100},
+	{ 10, SDL_KEYDOWN,  21}, //SHIFT
+	{ 10, SDL_KEYDOWN,  50}, //2
+	{ 60, SDL_KEYUP  ,  50},
+	{ 10, SDL_KEYUP  ,  21},
+	{ 10, SDL_KEYDOWN,  42}, //*
+	{ 60, SDL_KEYUP  ,  42},
+	{ 10, SDL_KEYDOWN,  21}, //SHIFT
+	{ 10, SDL_KEYDOWN,  50}, //2
+	{ 60, SDL_KEYUP  ,  50},
+	{ 10, SDL_KEYUP  ,  21},
+	{ 10, SDL_KEYDOWN,  44}, //,
+	{ 60, SDL_KEYUP  ,  44},
+	{ 10, SDL_KEYDOWN,  56}, //8
+	{ 60, SDL_KEYUP  ,  56},
+	{ 10, SDL_KEYDOWN,  44}, //,
+	{ 60, SDL_KEYUP  ,  44},
+	{ 10, SDL_KEYDOWN,  49}, //1
+	{ 60, SDL_KEYUP  ,  49},
+	{ 10, SDL_KEYDOWN,  13}, //CR
+	{ 60, SDL_KEYUP  ,  13},
+	{  0,           0,   0}
 };
 
 static UI_MENU_CALLBACK(press_runstop_restore_callback)
@@ -56,10 +89,25 @@ static UI_MENU_CALLBACK(press_runstop_restore_callback)
     return NULL;
 }
 
+static UI_MENU_CALLBACK(type_load_callback)
+{
+    if (activated) {
+		SDL_CreateThread(ui_key_press_sequence, keyseq_load);
+        return sdl_menu_text_exit_ui;
+    }
+    return NULL;
+}
+
+
 const ui_menu_entry_t misc_menu[] = {
-    { "Press RUN/STOP + RESTORE",
+    { "RUN/STOP + RESTORE",
       MENU_ENTRY_OTHER,
       press_runstop_restore_callback,
       NULL },
+    { "LOAD\"*\",8,1",
+      MENU_ENTRY_OTHER,
+      type_load_callback,
+      NULL },
     SDL_MENU_LIST_END
 };
+ 
