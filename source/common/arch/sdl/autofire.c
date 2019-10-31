88adc19dd30b574e7f3c2215795a8396dde990a2
badda71 <webersebastian@yahoo.de>
2019-10-31 09:00:29 +0100

    bugfix: autofire wouldn't turn off

--- source/common/arch/sdl/autofire.c
+++ source/common/arch/sdl/autofire.c
@@ -29,6 +29,7 @@
  *
  */
 
+#include <3ds.h>
 #include "vice.h"
 #include "vice_sdl.h"
 #include "autofire.h"
@@ -41,10 +42,10 @@
 //The thread that will be used
 SDL_Thread *af_port1_thread = NULL;
 SDL_Thread *af_port2_thread = NULL;
-int af_key[2] = { 0, 0 };
-SDL_sem *af_start[2];
-SDL_sem *af_stop[2];
-int af_active[2] = {0,0};
+int af_key[2] = { 0 };
+Handle af_start[2] = { 0 };
+Handle af_stop[2] = { 0 };
+int af_active[2] = { 0 };
 int af_init = 0;
 int af_button_joy = 0;
 int af_button_ks1 = 0;
@@ -56,7 +57,8 @@ static int autofire_thread( void *data ) {
 	
 	//While the program is not over
 	while( 1 ) {
-		SDL_SemWait(af_start[port]);
+		svcWaitSynchronization(af_start[port], U64_MAX);
+		svcClearEvent(af_start[port]);
 		int s=500/joy_auto_speed;
 		int k=af_key[port];
 		switch (k >> 8) {
@@ -72,12 +74,13 @@ static int autofire_thread( void *data ) {
 				e1.key.keysym.unicode = e1.key.keysym.sym =
 				e2.key.keysym.unicode = e2.key.keysym.sym = k & 0xff;
 		}
-		while(SDL_SemTryWait(af_stop[port]) && (!sdl_menu_state || events_to_emu)) {
+		while(svcWaitSynchronization(af_stop[port], 0) && (!sdl_menu_state || events_to_emu)) {
 			SDL_PushEvent(&e1);
 			SDL_Delay(s);
 			SDL_PushEvent(&e2);
 			SDL_Delay(s);
 		}
+		svcClearEvent(af_stop[port]);
 	}
 	return 0;
 }
@@ -90,10 +93,10 @@ static int autofire_init() {
 
 
 	// start autofire threads
-	af_start[0] = SDL_CreateSemaphore(0);
-	af_start[1] = SDL_CreateSemaphore(0);	
-	af_stop[0] = SDL_CreateSemaphore(0);
-	af_stop[1] = SDL_CreateSemaphore(0);	
+	svcCreateEvent(&af_start[0],0);
+	svcCreateEvent(&af_start[1],0);
+	svcCreateEvent(&af_stop[0],0);
+	svcCreateEvent(&af_stop[1],0);
 	int i1=0,i2=1;
 	if ((af_port1_thread = SDL_CreateThread(autofire_thread, &i1)) == NULL ) return -1;
 	if ((af_port2_thread = SDL_CreateThread(autofire_thread, &i2)) == NULL ) return -1;
@@ -105,10 +108,10 @@ void autofire_shutdown() {
 	if (af_init) {
 		if (af_port1_thread) SDL_KillThread (af_port1_thread);
 		if (af_port2_thread) SDL_KillThread (af_port2_thread);
-		if(af_start[0]) SDL_DestroySemaphore(af_start[0]);
-		if(af_start[1]) SDL_DestroySemaphore(af_start[1]);
-		if(af_stop[0]) SDL_DestroySemaphore(af_stop[0]);
-		if(af_stop[1]) SDL_DestroySemaphore(af_stop[1]);
+		if(af_start[0]) svcCloseHandle(af_start[0]);
+		if(af_start[1]) svcCloseHandle(af_start[1]);
+		if(af_stop[0]) svcCloseHandle(af_stop[0]);
+		if(af_stop[1]) svcCloseHandle(af_stop[1]);
 	}
 	af_init=0;
 }
@@ -135,13 +138,13 @@ int start_autofire(int port) {
 
 	if ((af_key[port]=af_get_joydev_fire(port))==0) return -1;
 	// clear stop semaphore
-	while(!SDL_SemTryWait(af_stop[port]));
-	SDL_SemPost(af_start[port]);
+	svcClearEvent(af_stop[port]);
+	svcSignalEvent(af_start[port]);
 	return 0;
 }
 
 void stop_autofire(int port) {
 	// clear start semaphore
-	while(!SDL_SemTryWait(af_start[port]));
-	SDL_SemPost(af_stop[port]);
+	svcClearEvent(af_start[port]);
+	svcSignalEvent(af_stop[port]);
 }
